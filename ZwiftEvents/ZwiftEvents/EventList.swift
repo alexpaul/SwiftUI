61e5129cea7df53e368752a25bb0f30b9bd29b6d
Alex Paul <alexpaul@users.noreply.github.com>
2022-11-21 07:00:05 -0500

    Added

---
+++ ZwiftEvents/ZwiftEvents/EventList.swift
@@ -0,0 +1,20 @@
+//
+//  EventList.swift
+//  ZwiftEvents
+//
+//  Created by Alex Paul on 11/21/22.
+//
+
+import SwiftUI
+
+struct EventList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventList_Previews: PreviewProvider {
+    static var previews: some View {
+        EventList()
+    }
+}
